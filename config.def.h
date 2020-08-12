6d0f7058f0b6348db575f9fd24a998264ab8e2df
Devlin McClure <mccluredt@gmail.com>
2020-08-12 19:00:04 -0400

    Change colors to dracula theme, font to FIra Code

--- config.def.h
+++ config.def.h
@@ -9,17 +9,18 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Fira Code:size=12" };
+static const char dmenufont[]       = "Fira Code:size=12";
+static const char normbgcolor[]     = "#282a36";
+static const char normbgborder[]    = "#ff79c6";
+static const char normfgcolor[]     = "#f8f8f2";
+static const char selfgcolor[]      = "#6272a4";
+static const char selbordercolor[]  = "#bd93f9";
+static const char selbgcolor[]      = "#44475a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbgborder },
+	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
