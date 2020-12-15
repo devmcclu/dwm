209dab40543bff23fc36aa8b567b63109c60010f
Devlin <mccluredt@gmail.com>
2020-12-15 17:44:47 -0500

    Many changes, change dmenu to rofi

--- config.def.h
+++ config.def.h
@@ -67,10 +67,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-h", "32", NULL };
 static const char *termcmd[]  = { "kitty", NULL};
+static const char *roficmd[]  = { "rofi", "-show", "combi", "NULL" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
